787f16cefc3e4d41f666018c466230cecb009d40
glaszig <glaszig@gmail.com>
2020-07-06 23:55:55 +0200

    moved response building into separate class

--- lib/rack/identicon.rb
+++ lib/rack/identicon.rb
@@ -1,7 +1,6 @@
 require "rack/identicon/version"
 require "rack/identicon/caching"
-require "digest"
-require "identicon"
+require "rack/identicon/response"
 
 module Rack
   module Identicon
@@ -9,56 +8,14 @@ module Rack
 
     include Caching
 
-    DEFAULT_SIZE = 128
-    DEFAULT_BG   = "ffffff".freeze
-
     def self.new *args
       Middleware.new *args
     end
 
     class Middleware
       def call env
-        request = Rack::Request.new env
-
-        if valid_request? request
-          [ 200, { "Content-Type" => "image/png" }, [ response_body(request) ] ]
-        else
-          [ 404, { "Content-Type" => "text/plain" }, [ "Not Found" ] ]
-        end
+        Response.new(env).triplet
       end
-
-      protected
-
-        def valid_request? request
-          request.path_info[1..-1].strip.length > 0
-        end
-
-        def response_body request
-          Rack::Identicon.cache cache_key(request) do
-            data = extract_payload request
-            size = extract_size request
-            bg   = extract_background request
-
-            ::Identicon.blob_for(data, size.to_i, bg)
-          end
-        end
-
-        def extract_payload request
-          Digest::SHA2.hexdigest request.path_info[1..-1]
-        end
-
-        def extract_size request
-          request.params["s"] || DEFAULT_SIZE
-        end
-
-        def extract_background request
-          color = request.params.fetch "b", DEFAULT_BG
-          color.scan(/.{2}/).map { |c| c.to_i 16 }
-        end
-
-        def cache_key request
-          Digest::SHA2.hexdigest request.path_info + request.query_string
-        end
     end
   end
 end
---
+++ lib/rack/identicon/response.rb
@@ -0,0 +1,55 @@
+require "digest"
+require "identicon"
+
+module Rack
+  module Identicon
+    class Response
+      DEFAULT_SIZE = 128
+      DEFAULT_BG   = "ffffff".freeze
+
+      def initialize env
+        @request = Rack::Request.new env
+        @size = @request.params["s"] || DEFAULT_SIZE
+        @bg = parse_color @request.params.fetch "b", DEFAULT_BG
+      end
+
+      def body
+        cache { ::Identicon.blob_for hashed_payload, @size.to_i, @bg }
+      end
+
+      def triplet
+        if valid_request?
+          [ 200, { "Content-Type" => "image/png" }, [ body ] ]
+        else
+          [ 404, { "Content-Type" => "text/plain" }, [ "Not Found" ] ]
+        end
+      end
+
+      protected
+
+        def valid_request?
+          payload.length > 0
+        end
+
+        def hashed_payload
+          @hashed_payload ||= Digest::SHA2.hexdigest(payload)
+        end
+
+        def payload
+          @payload ||= @request.path_info[1..-1].strip
+        end
+
+        def parse_color hex_str
+          hex_str.scan(/.{2}/).map { |c| c.to_i 16 }
+        end
+
+        def cache &blk
+          Rack::Identicon.cache cache_key, &blk
+        end
+
+        def cache_key
+          hashed_payload + "?s=#{@size}&b=#{@bg}"
+        end
+    end
+  end
+end
